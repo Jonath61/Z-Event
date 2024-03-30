9b55e03ad5bd404a7a3dcf786be7cab33ecf1d14
Jonath61 <jonath.even@gmail.com>
2024-03-30 17:46:48 +0100

    update

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,7 +12,6 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Contracts\Service\Attribute\Required;
 
 #[Route('/api', name: 'app_api_')]
 class SecurityController extends AbstractController
@@ -22,30 +21,16 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/register', name: 'register', methods: 'POST')]
-    /** @OA\(
-     *     path="/api/register",
-     *     summary="Inscription d'un nouvel utilisateur",
-     *     @OA\RequestBody(
-     *         required=true,
-     *         description="Données de l'utilisateur à inscrire",
-     *         @OA\JsonContent(
-     *             type="object",
-     *             @OA\Property(property="email", type="string", example="adresse@email.com"),
-     *             @OA\Property(property="password", type="string", example="Mot de passe")
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response=201,
-     *         description="Utilisateur inscrit avec succès",
-     *         @OA\JsonContent(
-     *             type="object",
-     *             @OA\Property(property="user", type="string", example="Nom d'utilisateur"),
-     *             @OA\Property(property="apiToken", type="string", example="31a023e212f116124a36af14ea0c1c3806eb9378"),
-     *             @OA\Property(property="roles", type="array", @OA\Items(type="string", example="ROLE_USER"))
-     *         )
-     *     )
-     * )
-     */
+    #[OA\Post(
+        path: "/api/register",
+        summary: "Inscription d'un nouvel utilisateur",
+    )]
+    #[OA\RequestBody(
+        description: "Données de l'utilisateur à inscrire",
+        required: true,
+        content: new Model(type: User::class)
+    )]
+    #[OA\Response( response: 200, description: 'Successful response', content: new Model(type: User::class) )]
 
     public function register(Request $request, UserPasswordHasherInterface $passwordHasher): JsonResponse
     {
@@ -63,6 +48,16 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/login', name: 'login', methods: 'POST')]
+    #[OA\Post(
+        path: "/api/login",
+        summary: "Connecter un utilisateur"
+    )]
+    #[OA\RequestBody(
+        description: "Données de l'utilisateur",
+        required: true,
+        content: new Model(type: User::class)
+    )]
+    #[OA\Response( response: 200, description: 'Successful response', content: new Model(type: User::class) )]
 
     public function login(#[CurrentUser] ?User $user): JsonResponse
     {
